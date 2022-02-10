771c29d613b690022dd125c04f36a0450eb35ad1
jabber <2898117012@qq.com>
2022-02-10 21:05:33 +0800

    init

---
+++ src/Providers/BillingServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dealskoo\Billing\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BillingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
