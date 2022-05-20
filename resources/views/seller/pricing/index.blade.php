87e2ac7667f9ce5e7716904bb31009a369feaf79
jabber <2898117012@qq.com>
2022-05-20 22:34:18 +0800

    init

--- resources/views/seller/pricing/index.blade.php
+++ resources/views/seller/pricing/index.blade.php
@@ -2,5 +2,96 @@
 
 @section('title',__('billing::billing.pricing'))
 @section('body')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a
+                                href="{{ route('seller.dashboard') }}">{{ __('seller::seller.dashboard') }}</a></li>
+                        <li class="breadcrumb-item"><a
+                                href="javascript: void(0);">{{ __('billing::billing.billing') }}</a></li>
+                        <li class="breadcrumb-item active">{{ __('billing::billing.pricing') }}</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">{{ __('billing::billing.pricing') }}</h4>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-xxl-10">
 
+            <!-- Pricing Title-->
+            <div class="text-center">
+                <h3 class="mb-2">Our Plans and Pricing</h3>
+                <p class="text-muted w-50 m-auto">
+                    We have plans and prices that fit your business perfectly.
+                </p>
+            </div>
+
+            <!-- Plans -->
+            <div class="row mt-sm-5 mt-3 mb-3">
+                <div class="col-md-4">
+                    <div class="card card-pricing">
+                        <div class="card-body text-center">
+                            <p class="card-pricing-plan-name fw-bold text-uppercase">Lite</p>
+                            <i class="card-pricing-icon dripicons-user text-primary"></i>
+                            <h2 class="card-pricing-price">$19 <span>/ Month</span></h2>
+                            <ul class="card-pricing-features">
+                                <li>10 GB Storage</li>
+                                <li>500 GB Bandwidth</li>
+                                <li>No Domain</li>
+                                <li>1 User</li>
+                                <li>Email Support</li>
+                                <li>24x7 Support</li>
+                            </ul>
+                            <button class="btn btn-primary mt-4 mb-2 rounded-pill">Choose Plan</button>
+                        </div>
+                    </div> <!-- end Pricing_card -->
+                </div> <!-- end col -->
+
+                <div class="col-md-4">
+                    <div class="card card-pricing card-pricing-recommended">
+                        <div class="card-body text-center">
+                            <div class="card-pricing-plan-tag">Recommended</div>
+                            <p class="card-pricing-plan-name fw-bold text-uppercase">Standard</p>
+                            <i class="card-pricing-icon dripicons-briefcase text-primary"></i>
+                            <h2 class="card-pricing-price">$29 <span>/ Month</span></h2>
+                            <ul class="card-pricing-features">
+                                <li>50 GB Storage</li>
+                                <li>900 GB Bandwidth</li>
+                                <li>2 Domain</li>
+                                <li>10 User</li>
+                                <li>Email Support</li>
+                                <li>24x7 Support</li>
+                            </ul>
+                            <button class="btn btn-primary mt-4 mb-2 rounded-pill">Choose Plan</button>
+                        </div>
+                    </div> <!-- end Pricing_card -->
+                </div> <!-- end col -->
+
+                <div class="col-md-4">
+                    <div class="card card-pricing">
+                        <div class="card-body text-center">
+                            <p class="card-pricing-plan-name fw-bold text-uppercase">Advanced</p>
+                            <i class="card-pricing-icon dripicons-store text-primary"></i>
+                            <h2 class="card-pricing-price">$39 <span>/ Month</span></h2>
+                            <ul class="card-pricing-features">
+                                <li>100 GB Storege</li>
+                                <li>Unlimited Bandwidth</li>
+                                <li>10 Domain</li>
+                                <li>Unlimited User</li>
+                                <li>Email Support</li>
+                                <li>24x7 Support</li>
+                            </ul>
+                            <button class="btn btn-primary mt-4 mb-2 rounded-pill">Choose Plan</button>
+                        </div>
+                    </div> <!-- end Pricing_card -->
+                </div> <!-- end col -->
+
+            </div>
+            <!-- end row -->
+
+        </div> <!-- end col-->
+    </div>
 @endsection
